46eeff245c75d76ec55be59c84a1ce3cb9578111
erenjax <erenjax@gmail.com>
2022-07-06 15:32:33 -0400

    Max input length of 4, Input accepts 0

--- new-app/src/App/runGame/InputGuess.tsx
+++ new-app/src/App/runGame/InputGuess.tsx
@@ -1,46 +1,45 @@
-import React, {useState} from 'react';
-import * as Code from '../../model/code';
+import React, { useState } from "react";
+import * as Code from "../../model/code";
 
 const InputGuess = () => {
-  const [guessString, setGuessString] = useState('');
-  const [printGuess, setPrintGuess] = useState('');
+  const [guessString, setGuessString] = useState("");
+  const [printGuess, setPrintGuess] = useState("");
 
   const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newGuess = event.target.value;
     setGuessString(newGuess);
   };
-  
-  const submitGuess = () : void => {
-    const codeForm: Code.Code = Code.fromString(guessString)
-    const stringForm: string = Code.codeToString(codeForm)
-    setPrintGuess(stringForm)
-  }
+
+  const submitGuess = (): void => {
+    const codeForm: Code.Code = Code.fromString(guessString);
+    const stringForm: string = Code.codeToString(codeForm);
+    setPrintGuess(stringForm);
+  };
 
   const handleOnKeyPress = (event: React.KeyboardEvent): void => {
-    if (event.key === 'Enter'){
-      submitGuess()
+    if (event.key === "Enter") {
+      submitGuess();
     }
   };
   const handleOnClick = () => {
-    submitGuess()
-  }
-   
-  return(
+    submitGuess();
+  };
+
+  return (
     <div>
-       <input
+      <input
         value={guessString}
         onChange={handleInput}
         placeholder="Enter a 4-digit number"
         className="input"
+        maxLength={4}
         onKeyPress={handleOnKeyPress}
       />
-      <button onClick={handleOnClick}>
-        Submit
-      </button>
+      <button onClick={handleOnClick}>Submit</button>
       <p>Press Enter to submit a guess</p>
       <p>Your Guess: {printGuess}</p>
     </div>
   );
-}
+};
 
-export default InputGuess; 
+export default InputGuess;
