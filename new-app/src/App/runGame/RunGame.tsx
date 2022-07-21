aaa70cda5c571e26dcf866b4d4b5052cd9387fa1
Emily Jackson <erenjax@gmail.com>
2022-07-21 17:26:31 -0400

    so much styling
    - add breaking points for mobile view

--- new-app/src/App/runGame/RunGame.tsx
+++ new-app/src/App/runGame/RunGame.tsx
@@ -3,10 +3,10 @@ import * as Code from '../../model/code';
 import RandomNumber from "./RandNum";
 import OtpInputGuess from "./OTPInputGuess";
 import Hints from "./hints";
+import RenderGuess from "./RenderGuess";
 
 const RunGame = () => {
   const [guess, setGuess] = useState(new Array(4).fill(""))
-
   const defaultCode: Code.Code = [0, 0, 0, 0]
   const [randomNumber, setRandomNumber] = useState(defaultCode)
   const RandomNumberProps = {
@@ -15,11 +15,14 @@ const RunGame = () => {
   }
 
   return (
-    <div>
-      <h1>Nordle</h1>
-      <RandomNumber  {...RandomNumberProps} />
-      <OtpInputGuess guess={guess} setGuess={setGuess} />
-      <Hints guess={guess} randomNumber={randomNumber} />
+    <div className="app-runGame">
+      <div className="flex flex-col justify-center bg-charcoal border-8 border-hacky-green rounded-3xl h-full w-full p-6 md:p-16">
+        <h1>Nordle</h1>
+        <RandomNumber  {...RandomNumberProps} />
+        <RenderGuess randomNumber={randomNumber} guess={[1, 0, 1, 0]} />
+        <OtpInputGuess guess={guess} setGuess={setGuess} />
+        <Hints guess={guess} randomNumber={randomNumber} />
+      </div>
     </div>
   );
 }
